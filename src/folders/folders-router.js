52a1047955187d0362fc3119d803c10e663cd173
Sarah Drew <sarahdrew93@gmail.com>
2019-05-11 15:54:30 -0500

    deleted extra export

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -94,67 +94,66 @@ foldersRouter
             .catch(next);
     });
 
-module.exports = foldersRouter;
 
-foldersRouter
-    .route('/api/folders/:folderId')
-    .all((req, res, next) => {
-        FoldersService.getById(
-            req.app.get('db'),
-            req.params.folder_id)
+// foldersRouter
+//     .route('/api/folders/:folderId')
+//     .all((req, res, next) => {
+//         FoldersService.getById(
+//             req.app.get('db'),
+//             req.params.folder_id)
 
-            .then(folder => {
-                if (!folder) {
-                    return res.status(404).json({
-                        error: { message: `Folder doesn't exist` }
-                    })
-                }
-                res.folder = folder
-                next()
-            })
-            .catch(next)
-    })
-    .get((req, res, next) => {
-        const knexInstance = req.app.get('db');
-        FoldersService.getAllFolders(knexInstance)
-            .then(folders => {
-                return res.json(folders.map(folder => sanitizeFolder(folder)));
-            })
-            .catch(next)
-            .patch(jsonParser, (req, res, next) => {
-                const { name } = req.body
-                const folderToUpdate = { name }
+//             .then(folder => {
+//                 if (!folder) {
+//                     return res.status(404).json({
+//                         error: { message: `Folder doesn't exist` }
+//                     })
+//                 }
+//                 res.folder = folder
+//                 next()
+//             })
+//             .catch(next)
+//     })
+//     .get((req, res, next) => {
+//         const knexInstance = req.app.get('db');
+//         FoldersService.getAllFolders(knexInstance)
+//             .then(folders => {
+//                 return res.json(folders.map(folder => sanitizeFolder(folder)));
+//             })
+//             .catch(next)
+//             .patch(jsonParser, (req, res, next) => {
+//                 const { name } = req.body
+//                 const folderToUpdate = { name }
 
-                const numberOfValues = Object.values(folderToUpdate).filter(Boolean).length
-                if (numberOfValues === 0)
-                    return res.status(400).json({
-                        error: {
-                            message: `Request body must contain either 'name'`
-                        }
-                    })
+//                 const numberOfValues = Object.values(folderToUpdate).filter(Boolean).length
+//                 if (numberOfValues === 0)
+//                     return res.status(400).json({
+//                         error: {
+//                             message: `Request body must contain either 'name'`
+//                         }
+//                     })
 
-                FoldersService.updateFolders(
-                    req.app.get('db'),
-                    req.params.folder_id,
-                    folderToUpdate
-                )
-                    .then(numRowsAffected => {
-                        res.status(204).end()
-                    })
-                    .catch(next)
-            })
+//                 FoldersService.updateFolders(
+//                     req.app.get('db'),
+//                     req.params.folder_id,
+//                     folderToUpdate
+//                 )
+//                     .then(numRowsAffected => {
+//                         res.status(204).end()
+//                     })
+//                     .catch(next)
+//             })
 
-            .delete((req, res, next) => {
-                FoldersService.deleteFolder(
-                    req.app.get('db'),
-                    req.params.folderId)
-                    .then(numRowsAffected => {
-                        res.status(204).end()
-                    })
-                    .catch(next);
-            });
+//             .delete((req, res, next) => {
+//                 FoldersService.deleteFolder(
+//                     req.app.get('db'),
+//                     req.params.folderId)
+//                     .then(numRowsAffected => {
+//                         res.status(204).end()
+//                     })
+//                     .catch(next);
+//             });
 
-    })
+// })
 
 
 module.exports = foldersRouter;
